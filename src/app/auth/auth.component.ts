9c640a6b1a6395d301a0251c10c938fd36b291bf
Kyle <sdbagel@gmail.com>
2020-09-06 20:23:37 -0700

    Auth component login

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { StorageService } from '../core/services';
+
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
+  token: string;
   
-  constructor(private router: Router) { }
+  constructor(private storage: StorageService,
+              private router: Router) { }
 
   ngOnInit(): void { }
 
+  tryAuthorize(): void {
+    // In a real OAuth context, retrieve the token based
+    // off of a server running using the developer key.
+    this.storage.set("oauth_token", this.token);
+    console.log(this.storage.get("oauth_token"));
+
+    // Redirect back
+    this.router.navigateByUrl("/");
+  }
 }
