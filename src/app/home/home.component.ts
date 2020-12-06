38ad522c75c8312112baf9e4111345c22cb27a33
Kyle <sdbagel@gmail.com>
2020-12-06 00:08:40 -0800

    Add date headers to todo list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,24 +13,29 @@ import { TodoGeneric } from '../core/services/canvas/user/user';
 export class HomeComponent implements OnInit {
   courses: Course[];
   todo: TodoGeneric[];
-  stream: {
-    [id: string]: TodoGeneric[];
-  }
+  stream: Array<{
+    id: string
+    items: TodoGeneric[];
+  }> = [];
 
   constructor(private router: Router, 
               private courseService: CourseService,
               private userService: UserService) { }
 
   async ngOnInit(): Promise<void> { 
-    this.courses = await this.courseService.listCourses();
+    //this.courses = await this.courseService.listCourses();
     this.todo = (await this.userService.getTodo()).sort((a, b) => {
       return (new Date(a.assignment.due_at).getTime()) - (new Date(b.assignment.due_at).getTime())
     });
 
     this.todo.forEach(t => {
-      if (t.assignment?.due_at) {
-        console.log(t.assignment?.due_at);
-      }
+      let date = new Date(t.assignment?.due_at).toDateString();
+      if (date === new Date().toDateString()) date = "Today";
+      const index = this.stream?.findIndex(i => i.id == date);
+      if (index == -1 || !index)
+        this.stream.push({ id: date, items: [t] });
+      else
+        this.stream[index].items.push(t);
     });
   }
 
