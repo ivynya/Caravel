96d4510f8b4462a12d007dce3d38ecf6125456e0
Kyle <sdbagel@gmail.com>
2021-01-07 23:03:48 -0800

    ng: Remove home course Accessor component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,11 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 
 import { HomeComponent } from './home.component';
-import { AccessorComponent } from './accessor/accessor.component';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [HomeComponent, AccessorComponent],
+  declarations: [HomeComponent],
   imports: [CommonModule, SharedModule, HomeRoutingModule]
 })
 export class HomeModule {}
