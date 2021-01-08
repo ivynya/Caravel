90bcff30dd35d89981806012eeb3530bc4f2eb62
Kyle <sdbagel@gmail.com>
2021-01-07 19:30:50 -0800

    fix: Homepage dashboard will now properly cache planner

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,18 +9,28 @@ import { Course, PlannerItem } from '../core/schemas';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  courses: Course[];
-  stream: Array<{
+  // Holds all stream items (planner) by date.
+  stream: {
     id: string
     items: PlannerItem[];
-  }> = [];
+    completed: PlannerItem[];
+  }[] = [];
+  // Records what portion of the stream is loaded.
+  streamState: { start: Date, end: Date };
 
   constructor(private userService: UserService) { }
 
-  async ngOnInit(): Promise<void> { 
+  async ngOnInit(): Promise<void> {
+    // Get current date and end date, rounded to 12AM.
+    // Set to get today and tomorrow (Canvas limit).
     let now = new Date();
-    let end = new Date(now.getTime() + 86400*1000*31);
+    now = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    let end = new Date(now.getTime() + 86400*1000*2);
+    
+    // Record what part of the stream is loaded.
+    this.streamState = { start: now, end: end };
 
+    // Get planner items in this interval.
     this.userService.getPlanner(now, end, items => {
       this.populateStream(items);
     });
@@ -28,17 +38,19 @@ export class HomeComponent implements OnInit {
 
   // Populates stream with events from API
   private populateStream(upcoming: PlannerItem[]): void {
+    this.stream = [];
     upcoming.forEach(item => {
       let date = this.formatDate(item.plannable_date);
 
       const index = this.stream?.findIndex(i => i.id == date);
       if (index == -1)
-        this.stream.push({ id: date, items: [item] });
+        this.stream.push({ id: date, items: [item], completed: [] });
       else
         this.stream[index].items.push(item);
     });
   }
 
+  // Formats a datestring into a human readable date.
   private formatDate(date: string): string {
     let f = new Date(date);
     let today = new Date();
