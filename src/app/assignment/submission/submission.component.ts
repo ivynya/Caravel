70e61a20b85b96a0def577124ce0ec087b7793fb
Kyle <sdbagel@gmail.com>
2021-01-11 23:35:55 -0800

    ng: Remove test notif tools from submission component

--- src/app/assignment/submission/submission.component.ts
+++ src/app/assignment/submission/submission.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
-import { NotificationService } from 'app/core/services';
 import { Submission } from '../../core/schemas';
 
 @Component({
@@ -13,8 +12,7 @@ export class SubmissionComponent implements OnChanges {
   previewUrl: SafeResourceUrl;
   type: string;
 
-  constructor(private sanitizer: DomSanitizer,
-              private notif: NotificationService) { }
+  constructor(private sanitizer: DomSanitizer) { }
 
   ngOnChanges(): void {
     this.type = this.convertTypeToReadable(this.submission?.submission_type);
@@ -34,20 +32,6 @@ export class SubmissionComponent implements OnChanges {
       default:
         return 'Unknown';
     }
-  }
-
-  long(): void {
-    this.notif.triggerActionLoading();
-  }
-
-  resolve(): void {
-    this.notif.triggerActionFinished();
-  }
-
-  num = 0;
-  notify(): void {
-    this.notif.triggerNotification("Notification "+this.num, 0);
-    this.num++;
   }
 
 }
