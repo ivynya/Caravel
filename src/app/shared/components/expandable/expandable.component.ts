d1bf9a4486982ddecee0d1974404f532c3bcb2ab
Kyle <sdbagel@gmail.com>
2021-02-13 02:07:08 -0800

    style: Add in-code option for big/small expandables

--- src/app/shared/components/expandable/expandable.component.ts
+++ src/app/shared/components/expandable/expandable.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class ExpandableComponent {
   @Input() name: string;
+  @Input() bold = false;
   @Input() opened = false;
   @Output() expanded: EventEmitter<void>;
 
