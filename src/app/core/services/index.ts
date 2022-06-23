28433236a2e33f91bef2221968a18b199c829fcf
Vyne <vyne@ivy.direct>
2022-06-23 12:35:55 -0700

    repo: remove electron

--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -1,7 +1,6 @@
 
 export * from './cache/cache.service';
 export * from './configuration/configuration.service';
-export * from './electron/electron.service';
 export * from './notification/notification.service';
 export * from './modal/modal.service';
 export * from './shortcut/shortcut.service';
