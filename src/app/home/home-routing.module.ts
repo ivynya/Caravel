8bde1e9d1e9ec0d0bd379a61b305e3a69b976f18
ivynya <vyne@ivy.direct>
2022-08-30 00:25:52 -0700

    feat: Add layout to dashboard

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { PrivateLayoutComponent } from '../shared/layouts';
 
 const routes: Routes = [
   {
     path: 'home',
-    component: HomeComponent
+    component: PrivateLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]
   }
 ];
 
