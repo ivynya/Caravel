33000b73cf3416c8ca0334467ccce466bf830b01
ivynya <vyne@ivy.direct>
2022-09-01 14:22:21 -0700

    feat: Substitute qrcode module

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { QRCodeModule } from 'angular2-qrcode';
 
 import { AccountRoutingModule } from './account-routing.module';
 
@@ -8,6 +7,7 @@ import { AccountComponent } from './account.component';
 import { SharedModule } from '../shared/shared.module';
 import { ConfiguratorComponent } from './configurator/configurator.component';
 import { InputModule, SelectModule, TilesModule } from 'carbon-components-angular';
+import { QRCodeModule } from 'angularx-qrcode';
 
 @NgModule({
   declarations: [AccountComponent, ConfiguratorComponent],
