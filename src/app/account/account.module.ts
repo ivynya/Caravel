29514c0b61e683713c0d3f78cc0b40d0af73bb91
ivynya <vyne@ivy.direct>
2022-09-01 14:05:29 -0700

    repo: Remove legacy input text

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -7,10 +7,10 @@ import { AccountRoutingModule } from './account-routing.module';
 import { AccountComponent } from './account.component';
 import { SharedModule } from '../shared/shared.module';
 import { ConfiguratorComponent } from './configurator/configurator.component';
-import { InputModule, TilesModule } from 'carbon-components-angular';
+import { InputModule, SelectModule, TilesModule } from 'carbon-components-angular';
 
 @NgModule({
   declarations: [AccountComponent, ConfiguratorComponent],
-  imports: [CommonModule, SharedModule, QRCodeModule, AccountRoutingModule, TilesModule, InputModule]
+  imports: [CommonModule, SharedModule, QRCodeModule, AccountRoutingModule, TilesModule, InputModule, SelectModule]
 })
 export class AccountModule {}
