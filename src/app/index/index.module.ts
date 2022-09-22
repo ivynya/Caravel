5a4fb47c0bfc80b1efb998dde712082350ef6795
ivynya <vyne@ivy.direct>
2022-09-21 20:19:22 -0700

    repo: slim build

--- src/app/index/index.module.ts
+++ src/app/index/index.module.ts
@@ -4,13 +4,12 @@ import { CommonModule } from '@angular/common';
 import { IndexRoutingModule } from './index-routing.module';
 
 import { IndexComponent } from './index.component';
-import { SharedModule } from '../shared/shared.module';
 
-import {HeaderModule, IconModule, SearchModule } from "carbon-components-angular";
+import { IconModule } from "carbon-components-angular";
 
 @NgModule({
   declarations: [IndexComponent],
-  imports: [CommonModule, SharedModule, IndexRoutingModule, HeaderModule, IconModule, SearchModule],
+  imports: [CommonModule, IndexRoutingModule, IconModule],
   providers: []
 })
 export class IndexModule {}
