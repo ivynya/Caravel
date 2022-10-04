2852e5050e17848c1ca884ffe54547cb7860bede
ivynya <vyne@ivy.direct>
2022-10-04 10:39:08 -0700

    feat: Course pages breadcrumbs

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
@@ -14,10 +15,12 @@ import { IconService } from 'carbon-components-angular';
   styleUrls: ['./course.component.scss']
 })
 export class CourseComponent implements OnInit {
+  breadcrumbs: { href?: string, content: string, current?: boolean }[];
   course: Course;
 
   constructor(private courseService: CourseService,
               private iconService: IconService,
+              private location: Location,
               private titleService: Title,
               private route: ActivatedRoute) {
   }
@@ -25,10 +28,23 @@ export class CourseComponent implements OnInit {
   ngOnInit(): void {
     this.iconService.registerAll([ArrowRight16, Checkmark16, Close16, Edit16]);
 
+    this.location.onUrlChange(url => {
+      this.breadcrumbs = [{ content: this.course.name, href: `/courses/${this.course.id}` }];
+      url = url.replace(`/courses/${this.course.id}`, "");
+      const fragments = url.split("/").splice(1);
+      fragments.pop(); // current page title is already shown
+      this.breadcrumbs.push(...fragments.map(fragment => {
+        // convert fragment into titlecase
+        fragment = fragment.replace(/^./, f => f.toUpperCase());
+        return { content: fragment, href: undefined, current: true }
+      }));
+    });
+
     this.route.params.subscribe(params => {
       this.courseService.getCourse(params.id, course => {
         this.course = course;
         this.titleService.setTitle(`${course.name} | Caravel`);
+        this.breadcrumbs = [{ content: this.course.name, href: `/courses/${this.course.id}` }];
       });
     });
   }
