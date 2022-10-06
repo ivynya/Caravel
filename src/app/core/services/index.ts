4e5da90a08e6731dc0121222bb5ca1fef06dc7d9
ivynya <vyne@ivy.direct>
2022-10-05 22:05:22 -0700

    repo: Remove legacy modal service

--- src/app/core/services/index.ts
+++ src/app/core/services/index.ts
@@ -1,6 +1,5 @@
 export * from "./cache/cache.service";
 export * from "./configuration/configuration.service";
 export * from "./notification/notification.service";
-export * from "./modal/modal.service";
 export * from "./shortcut/shortcut.service";
 export * from "./storage/storage.service";
