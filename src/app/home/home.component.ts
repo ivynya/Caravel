bd0109a62d68955229667311aafea4bf9083bea7
ivynya <vyne@ivy.direct>
2022-10-06 12:18:23 -0700

    feat: Use single date picker for dash / lint

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 import { UserService } from "../_core/services/canvas";
 import { PlannerItem } from "../_core/schemas";
@@ -6,140 +6,120 @@ import { PlannerItem } from "../_core/schemas";
 import { RoundDatePipe } from "../_core/pipes";
 
 @Component({
-	selector: "app-home",
-	templateUrl: "./home.component.html",
-	styleUrls: ["./home.component.scss"],
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent implements OnInit {
-	// Unprocessed stream items for sorting
-	private _streamItems: PlannerItem[][] = [];
-	// Records what portion of the stream is loaded.
-	streamState: [Date, Date];
-	// Holds all stream items (planner) by date.
-	stream: {
-		id: string;
-		items: PlannerItem[];
-		completed: PlannerItem[];
-	}[] = [];
-	// Enables "selection mode" for items on mobile
-	// SM only affects the component at < 512px.
-	selectionMode = false;
+export class HomeComponent {
+  // Records the start date of the stream
+  streamStart: Date;
+  _streamStart: [Date];
+  // Unprocessed stream items for sorting
+  private _streamItems: PlannerItem[][] = [];
+  // Holds all stream items (planner) by date.
+  stream: {
+    id: string;
+    items: PlannerItem[];
+    completed: PlannerItem[];
+  }[] = [];
+  // Enables "selection mode" for items @ < 512px width
+  selectionMode = false;
 
-	constructor(
-		private roundDate: RoundDatePipe,
-		private user: UserService
-	) {}
-
-	ngOnInit(): void {
-		this.loadFromToday();
+  constructor(private roundDate: RoundDatePipe, private user: UserService) {
+    this.loadFromToday();
 	}
 
-	// Toggles selection mode on todo components
-	toggleSelectionMode(): void {
-		this.selectionMode = !this.selectionMode;
-	}
+  // Toggles selection mode on todo components
+  toggleSelectionMode(): void {
+    this.selectionMode = !this.selectionMode;
+  }
 
-	load(): void {
-		// Reset and rebuild stream
-		this._streamItems = [];
-		this.stream = [];
+  // Adapts the ibm-datepicker model to a single Date
+  ibmDateAdapter(): void {
+    this.streamStart = this._streamStart[0];
+  }
 
-		// Load intervals
-		this.getItems(1);
-	}
+  // Reset and rebuild stream from streamStart
+  load(): void {
+    this._streamStart = [this.streamStart];
+    this._streamItems = [];
+    this.stream = [];
+    this.getItems(1);
+  }
 
-	loadFromToday(): void {
-		// Reset and rebuild stream
-		this._streamItems = [];
-		this.stream = [];
+  // Reset stream to start from today at 12AM
+  loadFromToday(): void {
+    this.streamStart = this.roundDate.transform(new Date());
+    this.load();
+  }
 
-		// Initialize streamstate with date, rounded to 12AM.
-		const now = this.roundDate.transform(new Date());
-		this.streamState = [now, now];
+  // Rebuild stream from one day before currently loaded
+  loadFromPreviousDay(): void {
+    this.streamStart = new Date(this.streamStart.getTime() - 86400000);
+    this.load();
+  }
 
-		// Load intervals
-		this.getItems(1);
-	}
+  private getItems(to: number) {
+    const next = new Date(this.streamStart.getTime() + 86400 * 1000 * 31);
+    this.user.getPlanner(this.streamStart, next, (res) => {
+      const items = res.data;
 
-	loadFromPreviousDay(): void {
-		// Reset and rebuild stream
-		this._streamItems = [];
-		this.stream = [];
+      // Add or overwrite added items to memory
+      this._streamItems[res.page] = items;
+      const concat = [].concat(...this._streamItems);
+      this.populateStream(concat);
 
-		// Reset stream period to previous date
-		const prev = new Date(this.streamState[0].getTime() - 86400000);
-		this.streamState = [prev, prev];
+      let unpopulated = true;
+      this.stream.forEach((day) => {
+        if (day.items.length > 0) unpopulated = false;
+      });
+      if (unpopulated && res.pagination?.next) to--;
+      res.pagination.next();
 
-		this.getItems(1);
-	}
+      // If target not reached, do a recursion
+      if (to > 0 && res.pagination?.next) to--;
+      res.pagination.next();
+    });
+  }
 
-	private getItems(to: number) {
-		const next = new Date(this.streamState[0].getTime() + 86400 * 1000 * 31);
-		this.user.getPlanner(this.streamState[0], next, (res) => {
-			const items = res.data;
+  // Populates stream with events from API
+  private populateStream(upcoming: PlannerItem[]): void {
+    this.stream = [];
+    upcoming.forEach((item) => {
+      const date = this.formatDate(item.plannable_date);
 
-			// Add or overwrite added items to memory
-			this._streamItems[res.page] = items;
-			const concat = [].concat(...this._streamItems);
-			this.streamState = [
-				this.streamState[0],
-				new Date(concat[concat.length - 1].plannable_date),
-			];
-			this.populateStream(concat);
+      let complete =
+        item.submissions.submitted ||
+        item.submissions.graded ||
+        item.planner_override?.dismissed ||
+        item.planner_override?.marked_complete ||
+        item.plannable?.workflow_state === "completed";
 
-			let unpopulated = true;
-			this.stream.forEach((day) => {
-				if (day.items.length > 0) unpopulated = false;
-			});
-			if (unpopulated && res.pagination?.next) to--;
-			res.pagination.next();
-
-			// If target not reached, do a recursion
-			if (to > 0 && res.pagination?.next) to--;
-			res.pagination.next();
-		});
-	}
-
-	// Populates stream with events from API
-	private populateStream(upcoming: PlannerItem[]): void {
-		this.stream = [];
-		upcoming.forEach((item) => {
-			const date = this.formatDate(item.plannable_date);
-
-			let complete = false;
-			if (
-				item.submissions.submitted ||
-				item.submissions.graded ||
-				item.planner_override?.dismissed ||
-				item.planner_override?.marked_complete ||
-				item.plannable?.workflow_state === "completed"
-			)
-				complete = true;
+      if (this.stream?.findIndex((i) => i.id == date) == -1)
+				this.stream.push({ id: date, items: [], completed: [] });
 
 			const index = this.stream?.findIndex((i) => i.id == date);
-			if (index == -1) {
-				if (complete)
-					this.stream.push({ id: date, items: [], completed: [item] });
-				else this.stream.push({ id: date, items: [item], completed: [] });
-			} else {
-				if (complete) this.stream[index].completed.push(item);
-				else this.stream[index].items.push(item);
-			}
-		});
-	}
+			if (complete) this.stream[index].completed.push(item);
+			else this.stream[index].items.push(item);
+    });
+  }
 
-	// Formats a datestring into a human readable date.
-	private formatDate(date: string): string {
-		const f = new Date(date);
-		const today = new Date();
-		const tmmrw = new Date(today.getTime() + 86400 * 1000);
-		const ystdy = new Date(today.getTime() - 86400 * 1000);
+  // Formats a datestring into a human readable date.
+  private formatDate(date: string): string {
+    const f = new Date(date);
+    const today = new Date();
+    const tmmrw = new Date(today.getTime() + 86400 * 1000);
+    const ystdy = new Date(today.getTime() - 86400 * 1000);
 
-		if (f.toDateString() === today.toDateString()) return "Today";
-		else if (f.toDateString() === tmmrw.toDateString())
-			return `Tomorrow (${f.toDateString()})`;
-		else if (f.toDateString() === ystdy.toDateString())
-			return `Yesterday (${f.toDateString()})`;
-		else return f.toDateString();
-	}
+    switch (f.toDateString()) {
+      case today.toDateString():
+        return "Today";
+      case tmmrw.toDateString():
+        return `Tomorrow (${f.toDateString()})`;
+      case ystdy.toDateString():
+        return `Yesterday (${f.toDateString()})`;
+      default:
+        return f.toDateString();
+    }
+  }
 }
